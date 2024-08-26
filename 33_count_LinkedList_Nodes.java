b13844293c2665b1b2963c335e9f6901b8d44684
NASARI NAGA SUNIL <167468256+nasarinagasunil@users.noreply.github.com>
2024-08-26 22:38:33 +0530

    Add files via upload

---
+++ 33_count_LinkedList_Nodes.java
@@ -0,0 +1,20 @@
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        // code here
+        int count=0;
+        Node temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            count++;
+        }
+        return count;
+    }
+}
