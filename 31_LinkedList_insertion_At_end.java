02679172e480f9df2e5e4a1b2fade4f2521f6f2b
NASARI NAGA SUNIL <167468256+nasarinagasunil@users.noreply.github.com>
2024-08-26 21:46:26 +0530

    Add files via upload

---
+++ 31_LinkedList_insertion_At_end.java
@@ -0,0 +1,22 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+        // code here
+        if(arr.length==0) return null;
+        Node head=new Node(arr[0]);
+        Node current=head;
+        for(int i=1;i<arr.length;i++){
+            current.next=new Node(arr[i]);
+            current=current.next;
+        }
+        return head;
+    }
+}
