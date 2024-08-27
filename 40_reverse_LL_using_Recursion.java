1c10462dddad92f581b8a3c1cb631047d9a81eaf
NASARI NAGA SUNIL <167468256+nasarinagasunil@users.noreply.github.com>
2024-08-28 00:29:24 +0530

    Add files via upload

---
+++ 40_reverse_LL_using_Recursion.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode newHead=reverseList(head.next);
+        ListNode front=head.next;
+        front.next=head;
+        head.next=null;
+        return newHead;
+        /**if(head==null || head.next==null){
+            return head;
+        }
+        ListNode temp=head;
+        ListNode prev=null;
+        ListNode nextNode;
+        while(temp!=null){
+            nextNode=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=nextNode;
+        }
+        return prev;
+        **/
+    }
+    
+}
