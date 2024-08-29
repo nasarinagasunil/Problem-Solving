015188fd52ec633b359f853cee40eff2ae66a82d
NASARI NAGA SUNIL <167468256+nasarinagasunil@users.noreply.github.com>
2024-08-29 10:14:05 +0530

    Add files via upload

---
+++ 41_Linked_List_Cycle.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+        /**ListNode temp=head;
+        HashMap<ListNode,Integer> map=new HashMap<>();
+        while(temp!=null){
+            if(map.containsKey(temp)){
+                return true;
+            }
+            map.put(temp,1);
+            temp=temp.next;
+        }
+        return false;
+        **/
+    }
+}
