528065a96015739f890cd76b284233a2ea7ee010
NASARI NAGA SUNIL <167468256+nasarinagasunil@users.noreply.github.com>
2024-09-19 11:56:44 +0530

    Add files via upload

---
+++ 55_Delete_all_occurrences_of_a_given_key_in_a_DLL.java
@@ -0,0 +1,38 @@
+
+
+// User function Template for Java
+
+/* Structure of Doubly Linked List
+class Node
+{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data)
+	{
+	    this.data = data;
+	    next = prev = null;
+	}
+}*/
+class Solution {
+    static Node deleteAllOccurOfX(Node head, int x) {
+        // Write your code here
+        Node temp=head;
+        while(temp!=null){
+            if(temp.data==x){
+                if(temp==head){
+                    head=head.next;
+                }
+                Node nextNode=temp.next;
+                Node prevNode=temp.prev;
+                if(nextNode!=null) nextNode.prev=prevNode;
+                if(prevNode!=null) prevNode.next=nextNode;
+                temp=nextNode;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
