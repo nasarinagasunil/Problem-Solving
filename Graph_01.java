ce6916268833ee11bb76f70ed7f260ac4b0d0010
NASARI NAGA SUNIL <167468256+nasarinagasunil@users.noreply.github.com>
2024-12-03 18:32:18 +0530

    Add files via upload

---
+++ Graph_01.java
@@ -0,0 +1,12 @@
+
+
+//User function Template for Java
+
+class Solution {
+    static long count(int n) {
+    // code here
+    int edges=(n*(n-1))/2;
+    return (long)Math.pow(2,edges);
+
+  }
+}
